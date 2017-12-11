29bd83313208e36b783472c5d9f69121d74e6ec8
selfresonator <selfresonator@gmail.com>
2017-12-10 23:46:27 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -6,64 +6,11 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    const MyContract = window.web3.eth.contract([
-      {
-        constant: true,
-        inputs: [],
-        name: 'getState',
-        outputs: [{ name: '', type: 'string' }],
-        payable: false,
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        constant: false,
-        inputs: [],
-        name: 'kill',
-        outputs: [],
-        payable: false,
-        stateMutability: 'nonpayable',
-        type: 'function',
-      },
-      {
-        constant: true,
-        inputs: [],
-        name: 'getSecret',
-        outputs: [{ name: '', type: 'string' }],
-        payable: false,
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        constant: false,
-        inputs: [{ name: 'newState', type: 'string' }],
-        name: 'setState',
-        outputs: [],
-        payable: true,
-        stateMutability: 'payable',
-        type: 'function',
-      },
-      {
-        constant: true,
-        inputs: [],
-        name: 'you_awesome',
-        outputs: [{ name: '', type: 'string' }],
-        payable: false,
-        stateMutability: 'view',
-        type: 'function',
-      },
-      {
-        inputs: [],
-        payable: false,
-        stateMutability: 'nonpayable',
-        type: 'constructor',
-      },
-      { payable: true, stateMutability: 'payable', type: 'fallback' },
-    ]);
+    const MyContract = window.web3.eth.contract([{"constant":false,"inputs":[],"name":"setExperimentInMotion","outputs":[{"name":"","type":"bool"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"getState","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"pseudoRandomResult","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getSecret","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newState","type":"string"}],"name":"setState","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"you_awesome","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":false,"name":"result","type":"bool"}],"name":"ExperimentComplete","type":"event"}]);
 
     this.state = {
       ContractInstance: MyContract.at(
-        '0x897efaf5b3a092ddf594abf9a75fb722d5feca71'
+        '0xbc6ccd5269e308b291ac1b3534a24a1349d7c077'
       ),
       contractState: '',
     };
@@ -71,6 +18,9 @@ class App extends Component {
     this.querySecret = this.querySecret.bind(this);
     this.queryContractState = this.queryContractState.bind(this);
     this.handleContractStateSubmit = this.handleContractStateSubmit.bind(this);
+    this.queryConditionResult = this.queryConditionResult.bind(this);
+    this.activateExperiment = this.activateExperiment.bind(this);
+    this.state.event = this.state.ContractInstance.ExperimentComplete();
   }
 
   querySecret() {
@@ -104,12 +54,43 @@ class App extends Component {
         value: window.web3.toWei(0.01, 'ether'),
       },
       (err, result) => {
-        console.log(`Smart contract state change initialized`);
+        console.log(`Smart contract STATE CHANGE initialized`);
+      }
+    );
+  }
+
+  queryConditionResult() {
+    const { pseudoRandomResult } = this.state.ContractInstance;
+
+    pseudoRandomResult((err, result) => {
+      console.log(
+        `This is what the fuck a smart contract looks like fool!:::: %$#@!!!!!${result}!!!!!@#$%`
+      );
+    });
+  }
+
+  activateExperiment() {
+    const { setExperimentInMotion } = this.state.ContractInstance;
+
+    setExperimentInMotion(
+      {
+        gas: 300000,
+        from: window.web3.eth.accounts[0],
+        value: window.web3.toWei(0.01, 'ether'),
+      },
+      (err, result) => {
+        console.log(`Smart contract EXPERIMENT initialized`);
       }
     );
   }
 
   render() {
+    this.state.event.watch((err, event) => {
+      if (err) console.log(`An error occured::: ${err}`);
+      console.log(`This is the event::: ${JSON.stringify(event)}`);
+      console.log(`This is the experiment result::: ${event.args.result}`);
+    });
+
     return (
       <div className="App">
         <header className="App-header">
@@ -140,6 +121,16 @@ class App extends Component {
           />
           <button type="submit"> Submit </button>
         </form>
+        <br />
+        <br />
+        <button onClick={this.activateExperiment}>
+          Activate the Experiment!!!
+        </button>
+        <br />
+        <br />
+        <button onClick={this.queryConditionResult}>
+          Query the Experiment Result
+        </button>
         <br />
         <br />
       </div>
